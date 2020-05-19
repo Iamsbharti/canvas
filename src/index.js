c5043f5fc2585be480e2ad9852d1d66e529f39e1
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-05-19 11:58:52 +0530

    udpated draw func

--- src/index.js
+++ src/index.js
@@ -19,10 +19,25 @@ let lastY = 0;
 function draw(e) {
   if (!isDrawing) return; //stop func from running when mouse is down
   console.log(e);
+  //draw
+  ctx.beginPath();
+  //start from
+  ctx.moveTo(lastX, lastY);
+  //end to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  //what to draw
+  ctx.stroke();
+  //update lastx and lasty value
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 //add event listeners
+//update mousedown event callback func to set lastx,y value to
+// the current one to avoid drawing from the last coordinate
+canvas.addEventListener("mousedown", event => {
+  isDrawing = true;
+  [lastX, lastY] = [event.offsetX, event.offsetY];
+});
 canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", () => (isDrawing = true));
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
